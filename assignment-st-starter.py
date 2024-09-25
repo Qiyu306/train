cb94f0be96c7bd5ac59077e1cb4eb60f0d3ba553
Qiyu306 <Dan2205918001@outlook.com>
2024-09-26 01:08:57 +0800

    Add files via upload

--- assignment-st-starter.py
+++ assignment-st-starter.py
@@ -2,7 +2,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-plt.style.use('seaborn')
+import seaborn as sns
+sns.set()
 
 # show the title
 st.title('Titanic App by Zhangjie Lai')
@@ -15,6 +16,8 @@ df = pd.read_csv('train.csv')
 # show the box plot for ticket price with different classes
 # you need to set the x labels and y labels
 # a sample diagram is shown below
+
+st.write(df[['city', 'country', 'population']])
 fig,ax= plt.subplots(1,3,figsize=(15,5))
 df[df['Pclass']==1].Fare.plot.box(ax=ax[0])
 ax[0].set_xlabel('Pclass=1')
@@ -28,3 +31,5 @@ df[df['Pclass']==3].Fare.plot.box(ax=ax[2])
 plt.xlabel('Fare')
 ax[2].set_xlabel('Pclass=3')
 
+
+st.pyplot(fig)
